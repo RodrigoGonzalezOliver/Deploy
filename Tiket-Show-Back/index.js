4e6b0a0613e880bce35a229216bb85af5128d1c4
rodrigo Gonzalez Oliver <rodrigo.gonzalez.oliver97@gmail.com>
2023-07-29 22:14:15 -0300

    sexto commit

--- Tiket-Show-Back/index.js
+++ Tiket-Show-Back/index.js
@@ -21,7 +21,7 @@
 const server = require("./src/app.js");
 const { conn } = require("./src/db.js");
 require("dotenv").config();
-const {PORT} = process.env;
+
 const {
   loadEvents,
   loadPlaces,
